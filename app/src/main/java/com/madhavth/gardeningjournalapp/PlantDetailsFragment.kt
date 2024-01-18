f3b9db2283272cbc9a1a5ef6b016774fcc11d21a
Madhav Thapa <madhavth77@gmail.com>
2024-01-17 20:12:21 -0800

    navvcontroller fixed

--- app/src/main/java/com/madhavth/gardeningjournalapp/PlantDetailsFragment.kt
+++ app/src/main/java/com/madhavth/gardeningjournalapp/PlantDetailsFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.navigation.fragment.navArgs
 import com.madhavth.gardeningjournalapp.features.plant_details.presentation.view_models.PlantDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class PlantDetailsFragment : Fragment() {
 
-    private val plantDetailViewModel by hiltNavGraphViewModels<PlantDetailViewModel>(R.navigation.nav_graph)
+    private val args: PlantDetailsFragmentArgs by navArgs()
+
+    private val plantDetailViewModel by hiltNavGraphViewModels<PlantDetailViewModel>(R.id.nav_graph_xml)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -23,6 +29,11 @@ class PlantDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plant_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val plants = args.plant.id
     }
 
     companion object {
--- app/src/main/java/com/madhavth/gardeningjournalapp/features/plant_details/presentation/view_models/PlantDetailViewModel.kt
+++ app/src/main/java/com/madhavth/gardeningjournalapp/features/plant_details/presentation/view_models/PlantDetailViewModel.kt
@@ -1,28 +1,29 @@
 package com.madhavth.gardeningjournalapp.features.plant_details.presentation.view_models
 
+import android.service.quicksettings.Tile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.madhavth.gardeningjournalapp.core.data.repositories.PlantRepository
 import com.madhavth.gardeningjournalapp.core.domain.entities.Plant
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import dagger.hilt.EntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class PlantDetailViewModel @Inject constructor(
-    private val plantRepository: PlantRepository,
-    @Assisted private val plantId: Int,
+    private val plantRepository: PlantRepository
 ) : ViewModel() {
-     fun getPlantById(): LiveData<Plant> {
+     fun getPlantById(plantId: Int): LiveData<Plant> {
         return plantRepository.getPlantById(plantId)
     }
     suspend fun updatePlant(plant: Plant) {
         plantRepository.update(plant)
     }
-
     suspend fun removePlant(plant: Plant) {
         plantRepository.delete(plant)
     }
-
 }
