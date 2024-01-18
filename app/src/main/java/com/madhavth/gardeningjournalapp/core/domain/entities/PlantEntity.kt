96f3b8fab7e803a366c1d258578d484abec7d2d4
Madhav Thapa <madhavth77@gmail.com>
2024-01-17 19:30:00 -0800

    added navhost fragments, parcelize plugin

--- app/src/main/java/com/madhavth/gardeningjournalapp/core/domain/entities/PlantEntity.kt
+++ app/src/main/java/com/madhavth/gardeningjournalapp/core/domain/entities/PlantEntity.kt
@@ -1,9 +1,13 @@
 package com.madhavth.gardeningjournalapp.core.domain.entities
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+import java.io.Serializable
 
 @Entity(tableName = "plants")
+@Parcelize
 data class Plant(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
@@ -11,4 +15,4 @@ data class Plant(
     val type: String,
     val wateringFrequency: Int,
     val plantingDate: String
-)
+): Parcelable
