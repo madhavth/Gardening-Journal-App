7e0a432ddfd7e5658e944ec3264ddcd02f32ef1c
Madhav Thapa <madhavth77@gmail.com>
2024-01-17 19:53:53 -0800

    navigation action

--- app/src/main/java/com/madhavth/gardeningjournalapp/HomeFragment.kt
+++ app/src/main/java/com/madhavth/gardeningjournalapp/HomeFragment.kt
@@ -6,9 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
+import androidx.navigation.fragment.findNavController
+import com.madhavth.gardeningjournalapp.core.domain.entities.Plant
 import com.madhavth.gardeningjournalapp.features.home.presentation.view_models.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -26,6 +27,13 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    fun navigateToPlantDetailsFragment(plant: Plant) {
+        val action = HomeFragmentDirections.actionHomeFragmentToPlantDetailsFragment(plant)
+        findNavController().navigate(action)
+    }
+    fun navigateToGardenLogFragment() {
+        findNavController().navigate(R.id.action_blankFragment_to_gardenLogFragment)
     }
 
     companion object {
